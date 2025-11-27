0fd297b9684a1e4d9f6208fe932720c572335c6b
Gitayespica <ariannayespica0@gmail.com>
2025-11-26 21:57:38 -0400

    Ejercicio 3: ProductController con CRUD

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+       'nombre' => 'required|string',
+       'descripcion' => 'required|string',
+       'precio' => 'required|numeric',
+       'stock' => 'required|integer',
+   ]);
+   return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+       'nombre' => 'required|string',
+       'descripcion' => 'required|string',
+       'precio' => 'required|numeric',
+       'stock' => 'required|integer',
+   ]);
+   $product->update($request->all());
+   return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $product->delete(); return response()->json(['message' => 'Producto eliminado']);
+    }
+}
